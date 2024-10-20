928409c9b95fefc5a2bff2877389b46c28d7840f
Mehak Yahya <168831879+MehakYahya@users.noreply.github.com>
2024-10-20 23:53:42 +0500

    Create app.config.ts

---
+++ src/app/app.config.ts
@@ -0,0 +1,13 @@
+import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import { provideRouter } from '@angular/router';
+
+import { routes } from './app.routes';
+
+export const appConfig: ApplicationConfig = {
+  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
+};
+
+export const AppSettings={
+  apiUrl: 'http://localhost:3000/',
+  appTitle: 'My Calculator Application'
+};
